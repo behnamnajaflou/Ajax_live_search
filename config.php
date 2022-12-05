1c8b7083541e8c7fe1ce4d2ba42c6c84537d110c
ben <ben@yahoo.com>
2022-12-05 18:19:36 +0100

    finished

--- config.php
+++ config.php
@@ -1,2 +1,11 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "Ajax_live_search";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if(!$conn){
+    echo "Connection failed" . mysqli_connect_error();
+}
 
--- search.php
+++ search.php
@@ -1,14 +1,5 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "Ajax_live_search";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
-if(!$conn){
-    echo "Connection failed" . mysqli_connect_error();
-}
-
+include "config.php";
 if(isset($_POST['input'])){
     $input = $_POST['input'];
 
